9a971b11c335848bebb8dfdbc19d7f51be9278d1
dhananjay yadav <ay815027@gmail.com>
2024-06-16 12:17:21 +0530

    change in cinfig file

--- wp-config.php
+++ wp-config.php
@@ -79,11 +79,11 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('FS_METHOD', true);
 
 /* That's all, stop editing! Happy publishing. */
 
